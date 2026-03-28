8eff1614677c85d6ebb615843b2e1eef2602eeb1
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2026-03-28 14:39:46 +0530

    Create api.js

---
+++ javascripts/discourse/lib/api.js
@@ -0,0 +1,19 @@
+export async function getLatest() {
+  return fetch("/latest.json").then(r => r.json());
+}
+
+export async function getReviewed() {
+  return fetch("/search.json?q=tags:reviewed").then(r => r.json());
+}
+
+export async function getFollowUp() {
+  return fetch("/search.json?q=tags:follow-up").then(r => r.json());
+}
+
+export async function getFlags() {
+  return fetch("/review.json?type=ReviewableFlaggedPost").then(r => r.json());
+}
+
+export async function searchUsers(term) {
+  return fetch(`/u/search/users.json?term=${term}`).then(r => r.json());
+}
